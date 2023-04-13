b6ca72e5e59d9daa03b0c68467b30d0a1ac40102
yusharnadi <yust.44@gmail.com>
2023-04-13 14:21:52 +0700

    add recently played

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js}'],
-    theme: {
-        extend: {
-            colors: {
-                dark: {
-                    100: '#7A8580',
-                    400: '#959D99',
-                    500: '#272727',
-                    900: '#171717',
-                },
-                primary: '#EE4950',
-                accent: '#F3777D',
-            },
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          100: "#7A8580",
+          400: "#959D99",
+          500: "#272727",
+          600: "#626A66",
+          900: "#171717",
         },
+        primary: "#EE4950",
+        accent: "#F3777D",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
